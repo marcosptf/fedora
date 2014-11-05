1ea77be80caaba4ce844b32f2cb47b37c74452df
marcosptf <marcosptf@yahoo.com.br>
2014-11-05 16:24:20 -0200

    Update python-devguide.sh

--- python-devguide.sh
+++ python-devguide.sh
@@ -4,8 +4,6 @@ virtualenv .venv
 
 .venv/bin/activate
 
-./configure --with-pydebug && make -j2
-
 pip install _bz2 
 pip install _dbm                  
 pip install _gdbm              
@@ -13,5 +11,7 @@ pip install _lzma
 pip install _sqlite3              
 pip install _tkinter           
 pip install readline 
+
+./configure --with-pydebug && make -j2
 
 ./python -m test -j3
