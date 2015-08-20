f4d13c858658c13d1d981d050f31137e5ed3c3fb
root <marcosptf@yahoo.com.br>
2015-08-20 08:11:15 -0300

    character counting c

---
+++ c/arrayCountDigitsWhiteSpaceOthers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+main() {
+  
+  int c, i, nwhite=0, others=0, ndigit[10];
+  
+  for(i=0;i< 10;i++){ndigit[i]=0;}
+  
+  while((c = getchar()) != EOF) {
+    
+    if(c >= '0' && c <= 9) {
+      ++ndigit[c-'0'];
+    }else if(c==' ' || c == '\n' || c == '\t') {
+      ++nwhite;
+    }else{
+      ++others;
+    }      
+  }
+  
+  printf("digits => ");
+  for(i=0;i< 10;i++){
+    printf(" %d", ndigit[i]);
+  }
+  printf(", white space => %d, other => %d \n", nwhite, others);
+  
+  
+}
