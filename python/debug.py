76830e0fb6336851134e821e969e9b44bfdd754c
Fedora Hacker p0w34007 <p0w34007@yahoo.com>
2015-08-21 17:49:50 -0300

    Create debug.py

---
+++ python/debug.py
@@ -0,0 +1,438 @@
+ipdb> 
+ipdb> 
+ipdb> 
+ipdb> ssl.AF_INET
+2
+ipdb> 
+2
+ipdb> ssl.ALERT_DESCRIPTION_ACCESS_DENIED
+49
+ipdb> 
+49
+ipdb> ssl.ALERT_DESCRIPTION_BAD_CERTIFICATE
+42
+ipdb> 
+42
+ipdb> ssl.ALERT_DESCRIPTION_BAD_CERTIFICATE_HASH_VALUE
+114
+ipdb> 
+114
+ipdb> ssl.ALERT_DESCRIPTION_BAD_CERTIFICATE_STATUS_RESPONSE
+113
+ipdb> 
+113
+ipdb> ssl.ALERT_DESCRIPTION_BAD_RECORD_MAC
+20
+ipdb> 
+20
+ipdb> ssl.ALERT_DESCRIPTION_CERTIFICATE_EXPIRED
+45
+ipdb> 
+45
+ipdb> ssl.ALERT_DESCRIPTION_CERTIFICATE_REVOKED
+44
+ipdb> 
+44
+ipdb> ssl.ALERT_DESCRIPTION_CERTIFICATE_UNKNOWN
+46
+ipdb> 
+46
+ipdb> ssl.ALERT_DESCRIPTION_CERTIFICATE_UNOBTAINABLE
+111
+ipdb> 
+111
+ipdb> ssl.ALERT_DESCRIPTION_CLOSE_NOTIFY
+0
+ipdb> 
+0
+ipdb> ssl.ALERT_DESCRIPTION_DECODE_ERROR
+50
+ipdb> 
+50
+ipdb> ssl.ALERT_DESCRIPTION_DECOMPRESSION_FAILURE
+30
+ipdb> 
+30
+ipdb> ssl.ALERT_DESCRIPTION_DECRYPT_ERROR
+51
+ipdb> 
+51
+ipdb> ssl.ALERT_DESCRIPTION_HANDSHAKE_FAILURE
+40
+ipdb> 
+40
+ipdb> ssl.ALERT_DESCRIPTION_ILLEGAL_PARAMETER
+47
+ipdb> 
+47
+ipdb> ssl.ALERT_DESCRIPTION_INSUFFICIENT_SECURITY
+71
+ipdb> 
+71
+ipdb> ssl.ALERT_DESCRIPTION_INTERNAL_ERROR
+80
+ipdb> 
+80
+ipdb> ssl.ALERT_DESCRIPTION_NO_RENEGOTIATION
+100
+ipdb> 
+100
+ipdb> ssl.ALERT_DESCRIPTION_PROTOCOL_VERSION
+70
+ipdb> 
+70
+ipdb> ssl.ALERT_DESCRIPTION_RECORD_OVERFLOW
+22
+ipdb> 
+22
+ipdb> ssl.ALERT_DESCRIPTION_UNEXPECTED_MESSAGE
+10
+ipdb> 
+10
+ipdb> ssl.ALERT_DESCRIPTION_UNKNOWN_CA
+48
+ipdb> 
+48
+ipdb> ssl.ALERT_DESCRIPTION_UNKNOWN_PSK_IDENTITY
+115
+ipdb> 
+115
+ipdb> ssl.ALERT_DESCRIPTION_UNRECOGNIZED_NAME
+112
+ipdb> 
+112
+ipdb> ssl.ALERT_DESCRIPTION_UNSUPPORTED_CERTIFICATE
+43
+ipdb> 
+43
+ipdb> ssl.ALERT_DESCRIPTION_UNSUPPORTED_EXTENSION
+110
+ipdb> 
+110
+ipdb> ssl.ALERT_DESCRIPTION_USER_CANCELLED
+90
+ipdb> 
+90
+ipdb> ssl.CERT_NONE
+0
+ipdb> 
+0
+ipdb> ssl.CERT_OPTIONAL
+1
+ipdb> 
+1
+ipdb> ssl.CERT_REQUIRED
+2
+ipdb> 
+2
+ipdb> ssl.CHANNEL_BINDING_TYPES
+['tls-unique']
+ipdb> 
+['tls-unique']
+ipdb> ssl.CertificateError
+<class 'ssl.CertificateError'>
+ipdb> 
+<class 'ssl.CertificateError'>
+ipdb> ssl.DER_cert_to_PEM_cert
+<function DER_cert_to_PEM_cert at 0x7f6ea679d398>
+ipdb> 
+<function DER_cert_to_PEM_cert at 0x7f6ea679d398>
+ipdb> ssl.DefaultVerifyPaths
+<class 'ssl.DefaultVerifyPaths'>
+ipdb> 
+<class 'ssl.DefaultVerifyPaths'>
+ipdb> ssl.HAS_ALPN
+False
+ipdb> 
+False
+ipdb> ssl.HAS_ECDH
+True
+ipdb> 
+True
+ipdb> ssl.HAS_NPN
+True
+ipdb> 
+True
+ipdb> ssl.HAS_SNI
+True
+ipdb> 
+True
+ipdb> ssl.OPENSSL_VERSION
+'OpenSSL 1.0.1k-fips 8 Jan 2015'
+ipdb> 
+'OpenSSL 1.0.1k-fips 8 Jan 2015'
+ipdb> ssl.OPENSSL_VERSION_INFO
+(1, 0, 1, 11, 15)
+ipdb> 
+(1, 0, 1, 11, 15)
+ipdb> ssl.OPENSSL_VERSION_NUMBER
+268439743L
+ipdb> 
+268439743L
+ipdb> ssl.OP_ALL
+2147484671
+ipdb> 
+2147484671
+ipdb> ssl.OP_CIPHER_SERVER_PREFERENCE
+4194304
+ipdb> 
+4194304
+ipdb> ssl.OP_NO_COMPRESSION
+131072
+ipdb> 
+131072
+ipdb> ssl.OP_NO_SSLv2
+16777216
+ipdb> 
+16777216
+ipdb> ssl.OP_NO_SSLv3
+33554432
+ipdb> 
+33554432
+ipdb> ssl.OP_NO_TLSv1
+67108864
+ipdb> 
+67108864
+ipdb> ssl.OP_NO_TLSv1_1
+268435456
+ipdb> 
+268435456
+ipdb> ssl.OP_NO_TLSv1_2
+134217728
+ipdb> 
+134217728
+ipdb> ssl.OP_SINGLE_DH_USE
+1048576
+ipdb> 
+1048576
+ipdb> ssl.OP_SINGLE_ECDH_USE
+524288
+ipdb> 
+524288
+ipdb> ssl.PEM_FOOTER
+'-----END CERTIFICATE-----'
+ipdb> 
+'-----END CERTIFICATE-----'
+ipdb> ssl.PEM_HEADER
+'-----BEGIN CERTIFICATE-----'
+ipdb> 
+'-----BEGIN CERTIFICATE-----'
+ipdb> ssl.PEM_cert_to_DER_cert
+<function PEM_cert_to_DER_cert at 0x7f6ea679d410>
+ipdb> 
+<function PEM_cert_to_DER_cert at 0x7f6ea679d410>
+ipdb> ssl.PROTOCOL_SSLv2
+0
+ipdb> 
+0
+ipdb> ssl.PROTOCOL_SSLv23
+2
+ipdb> 
+2
+ipdb> ssl.PROTOCOL_SSLv3
+1
+ipdb> 
+1
+ipdb> ssl.PROTOCOL_TLSv1
+3
+ipdb> 
+3
+ipdb> ssl.PROTOCOL_TLSv1_1
+4
+ipdb> 
+4
+ipdb> ssl.PROTOCOL_TLSv1_2
+5
+ipdb> 
+5
+ipdb> ssl.Purpose
+<class 'ssl.Purpose'>
+ipdb> 
+<class 'ssl.Purpose'>
+ipdb> ssl.RAND_add
+<built-in function RAND_add>
+ipdb> 
+<built-in function RAND_add>
+ipdb> ssl.RAND_egd
+<built-in function RAND_egd>
+ipdb> 
+<built-in function RAND_egd>
+ipdb> ssl.RAND_status
+<built-in function RAND_status>
+ipdb> 
+<built-in function RAND_status>
+ipdb> ssl.SOCK_STREAM
+1
+ipdb> 
+1
+ipdb> ssl.SOL_SOCKET
+1
+ipdb> 
+1
+ipdb> ssl.SO_TYPE
+3
+ipdb> 
+3
+ipdb> ssl.SSLContext
+<class 'ssl.SSLContext'>
+ipdb> 
+<class 'ssl.SSLContext'>
+ipdb> ssl.SSLEOFError
+<class 'ssl.SSLEOFError'>
+ipdb> 
+<class 'ssl.SSLEOFError'>
+ipdb> ssl.SSLError
+<class 'ssl.SSLError'>
+ipdb> 
+<class 'ssl.SSLError'>
+ipdb> ssl.SSLSocket
+<class 'ssl.SSLSocket'>
+ipdb> 
+<class 'ssl.SSLSocket'>
+ipdb> ssl.SSLSyscallError
+<class 'ssl.SSLSyscallError'>
+ipdb> 
+<class 'ssl.SSLSyscallError'>
+ipdb> ssl.SSLWantReadError
+<class 'ssl.SSLWantReadError'>
+ipdb> 
+<class 'ssl.SSLWantReadError'>
+ipdb> ssl.SSLWantWriteError
+<class 'ssl.SSLWantWriteError'>
+ipdb> 
+<class 'ssl.SSLWantWriteError'>
+ipdb> ssl.SSLZeroReturnError
+<class 'ssl.SSLZeroReturnError'>
+ipdb> 
+<class 'ssl.SSLZeroReturnError'>
+ipdb> ssl.SSL_ERROR_EOF
+8
+ipdb> 
+8
+ipdb> ssl.SSL_ERROR_INVALID_ERROR_CODE
+10
+ipdb> 
+10
+ipdb> ssl.SSL_ERROR_SSL
+1
+ipdb> 
+1
+ipdb> ssl.SSL_ERROR_SYSCALL
+5
+ipdb> 
+5
+ipdb> ssl.SSL_ERROR_WANT_CONNECT
+7
+ipdb> 
+7
+ipdb> ssl.SSL_ERROR_WANT_READ
+2
+ipdb> 
+2
+ipdb> ssl.SSL_ERROR_WANT_WRITE
+3
+ipdb> 
+3
+ipdb> ssl.SSL_ERROR_WANT_X509_LOOKUP
+4
+ipdb> 
+4
+ipdb> ssl.SSL_ERROR_ZERO_RETURN
+6
+ipdb> 
+6
+ipdb> ssl.VERIFY_CRL_CHECK_CHAIN
+12
+ipdb> 
+12
+ipdb> ssl.VERIFY_CRL_CHECK_LEAF
+4
+ipdb> 
+4
+ipdb> ssl.VERIFY_DEFAULT
+0
+ipdb> 
+0
+ipdb> ssl.VERIFY_X509_STRICT
+32
+ipdb> 
+32
+ipdb> ssl.VERIFY_X509_TRUSTED_FIRST
+32768
+ipdb> 
+32768
+ipdb> ssl.base64
+<module 'base64' from '/usr/lib64/python2.7/base64.pyc'>
+ipdb> 
+<module 'base64' from '/usr/lib64/python2.7/base64.pyc'>
+ipdb> ssl.cert_time_to_seconds
+<function cert_time_to_seconds at 0x7f6ea679d320>
+ipdb> 
+<function cert_time_to_seconds at 0x7f6ea679d320>
+ipdb> ssl.closing
+<class 'contextlib.closing'>
+ipdb> 
+<class 'contextlib.closing'>
+ipdb> ssl.create_connection
+<function create_connection at 0x7f6ea67d69b0>
+ipdb> 
+<function create_connection at 0x7f6ea67d69b0>
+ipdb> ssl.create_default_context
+<function create_default_context at 0x7f6ea678e938>
+ipdb> 
+<function create_default_context at 0x7f6ea678e938>
+ipdb> ssl.errno
+<module 'errno' (built-in)>
+ipdb> 
+<module 'errno' (built-in)>
+ipdb> ssl.get_default_verify_paths
+<function get_default_verify_paths at 0x7f6ea678e500>
+ipdb> 
+<function get_default_verify_paths at 0x7f6ea678e500>
+ipdb> ssl.get_protocol_name
+<function get_protocol_name at 0x7f6ea679d500>
+ipdb> 
+<function get_protocol_name at 0x7f6ea679d500>
+ipdb> ssl.get_server_certificate
+<function get_server_certificate at 0x7f6ea679d488>
+ipdb> 
+<function get_server_certificate at 0x7f6ea679d488>
+ipdb> ssl.match_hostname
+<function match_hostname at 0x7f6ea678e410>
+ipdb> 
+<function match_hostname at 0x7f6ea678e410>
+ipdb> ssl.namedtuple
+<function namedtuple at 0x7f6eb273cd70>
+ipdb> 
+<function namedtuple at 0x7f6eb273cd70>
+ipdb> ssl.os
+<module 'os' from '/usr/lib64/python2.7/os.pyc'>
+ipdb> 
+<module 'os' from '/usr/lib64/python2.7/os.pyc'>
+ipdb> ssl.re
+<module 're' from '/usr/lib64/python2.7/re.pyc'>
+ipdb> 
+<module 're' from '/usr/lib64/python2.7/re.pyc'>
+ipdb> ssl.socket
+<class 'socket._socketobject'>
+ipdb> 
+<class 'socket._socketobject'>
+ipdb> ssl.socket_error
+<class 'socket.error'>
+ipdb> 
+<class 'socket.error'>
+ipdb> ssl.sslwrap_simple
+<function sslwrap_simple at 0x7f6ea679d578>
+ipdb> 
+<function sslwrap_simple at 0x7f6ea679d578>
+ipdb> ssl.sys
+<module 'sys' (built-in)>
+ipdb> 
+<module 'sys' (built-in)>
+ipdb> ssl.textwrap
+<module 'textwrap' from '/usr/lib64/python2.7/textwrap.pyc'>
+ipdb> 
+<module 'textwrap' from '/usr/lib64/python2.7/textwrap.pyc'>
+ipdb> ssl.wrap_socket
+<function wrap_socket at 0x7f6ea67ae2a8>
+ipdb> 
