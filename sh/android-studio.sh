9d4484732930d1d66a68b2352291418a4598a271
marcosptf <marcosptf@yahoo.com.br>
2016-01-07 15:23:31 -0200

    Create android-studio.sh

---
+++ sh/android-studio.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export JAVA_HOME=/usr/java/jdk1.8.0_25
+echo "trying open android studio..."
+#cd /root/Downloads/android-studio/bin/
+#source studio.sh
+source `/root/Downloads/android-studio/bin/studio.sh`
