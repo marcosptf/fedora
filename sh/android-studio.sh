f8a6463c17cfef0369fb32924cd26e5d9d91b400
marcosptf <marcosptf@yahoo.com.br>
2016-02-10 18:53:53 -0200

    Update android-studio.sh

--- sh/android-studio.sh
+++ sh/android-studio.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export JAVA_HOME=/usr/java/jdk1.8.0_25
+export ANDROID_EMULATOR_FORCE_32BIT=true
 echo "trying open android studio..."
 #cd /root/Downloads/android-studio/bin/
 #source studio.sh
-source `/root/Downloads/android-studio/bin/studio.sh`
+source `/home/marcosptf/developer-master/android-studio/bin/studio.sh`
