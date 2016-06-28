2aa06078f9d8c0b0bae2acd72cfb042378872408
root <root@localhost.localdomain>
2016-06-28 19:52:50 -0300

    add flask ola mundo example

---
+++ python/flask-ola-mundo/manage.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    sistema de pontos
+
+    Esta aplicacao exemplo Flask, eh um simulador de sistema de espera.
+    Flask and sqlite3.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+
+def ola_mundo():
+    return 'aplicacao flask python, ola mundo!'
+  
+if __name__ == '__main__'  :
+    app.run()
+
