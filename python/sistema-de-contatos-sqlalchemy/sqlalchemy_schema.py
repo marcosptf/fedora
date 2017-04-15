859067b90c03d9ebb8667beb4f3c68366a8d296d
root <marcosptf@yahoo.com.br>
2017-04-15 19:36:35 -0300

    add sqlalchemy struture

---
+++ python/sistema-de-contatos-sqlalchemy/sqlalchemy_schema.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Integer, String
+
+class Contatos(Base):
+    __tablename__ = "contatos"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250)) #colocar not null
+    email = Column(String(50))
+    whatsapp = Column(String(12))
+    facebook = Column(String(50))
+    twitter = Column(String(50))
+    website = Column(String(50))
+    endereco = Column(String(250))
+    bairro = Column(String(50))
+    cidade = Column(String(50))
+    estado = Column(String(50))
+    
+    def __repr__(self):
+        return """
+        <Contatos(name='%s', email='%s', whatsapp='%s', facebook='%s', twitter='%s', website='%s',
+        endereco='%s',bairro='%s',cidade='%s',estado='%s')>
+        """ % (self.id, self.nome, self.email, self.whatsapp, self.facebook, self.twitter,self.website,
+        self.endereco, self.bairro, self.cidade, self.estado)
+
+
+
+
