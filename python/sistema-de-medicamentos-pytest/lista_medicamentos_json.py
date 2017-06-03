e655a1030958aba756a4e03ea24710d1e10179c9
root <marcosptf@yahoo.com.br>
2017-06-03 16:02:44 -0300

    fixing pytest

---
+++ python/sistema-de-medicamentos-pytest/lista_medicamentos_json.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+class medicamento:
+
+    def lista_medicamentos_json(self):
+
+        return {
+	    "medicamento": [{
+		"nome": "doril",
+		"preco": 3.50,
+		"quantidade-comprimidos": 20,
+		"desconto-aposentados": "True",
+		"desconto-gestantes": "True",
+		"farmacia-popular": "True",
+		"generico": "False",
+		"miligramas": "400mg"
+	    },{
+		"nome": "resfenol",
+		"preco": 4.00,
+		"quantidade-comprimidos": 10,
+		"desconto-aposentados": "True",
+		"desconto-gestantes": "True",
+		"farmacia-popular": "False",
+		"generico": "True",
+		"miligramas": "100mg"
+	    },{
+		"nome": "benegrip",
+		"preco": 7.50,
+		"quantidade-comprimidos": 30,
+		"desconto-aposentados": "True",
+		"desconto-gestantes": "True",
+		"farmacia-popular": "False",
+		"generico": "False",
+		"miligramas": "20mg"
+	    },{
+		"nome": "buscopan",
+		"preco": 5.50,
+		"quantidade-comprimidos": 20,
+		"desconto-aposentados": "True",
+		"desconto-gestantes": "True",
+		"farmacia-popular": "True",
+		"generico": "False",
+		"miligramas": "40mg"
+	    },{
+		"nome": "paracetamol",
+		"preco": 4.50,
+		"quantidade-comprimidos": 20,
+		"desconto-aposentados": "True",
+		"desconto-gestantes": "True",
+		"farmacia-popular": "True",
+		"generico": "True",
+		"miligramas": "40mg"
+	    },{
+		"nome": "neosaldina",
+		"preco": 3.00,
+		"quantidade-comprimidos": 10,
+		"desconto-aposentados": "False",
+		"desconto-gestantes": "False",
+		"farmacia-popular": "False",
+		"generico": "False",
+		"miligramas": "200mg"
+	    },{
+		"nome": "multigrip",
+		"preco": 6.50,
+		"quantidade-comprimidos": 20,
+		"desconto-aposentados": "True",
+		"desconto-gestantes": "False",
+		"farmacia-popular": "False",
+		"generico": "True",
+		"miligramas": "40mg"
+	    }]
+	}
+                
+    
+
