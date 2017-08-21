4280f24929cc2a6183381703ccdd58827cefbdeb
root <marcosptf@yahoo.com.br>
2017-08-21 06:07:57 -0300

    adding flask admin examples and labs

---
+++ python/flask-admin-labs/flask-admin-labs.py
@@ -0,0 +1,12 @@
+"""
++Capitulo 9 - Flask Login + Flask Admin + Flask Redis + Memcached + PostgreSql
+https://aws.amazon.com/pt/elasticache/what-is-redis/
+https://pypi.python.org/pypi/Flask-And-Redis/0.7
+https://pypi.python.org/pypi/Flask-Cache-Redis-Cluster/0.0.5
+https://pypi.python.org/pypi/Flask-PyMemcache/0.0.5
+https://pypi.python.org/pypi/Flask-Login/0.4.0
+https://pypi.python.org/pypi/Flask-Admin/1.5.0
+"""
+
+from Flask import flask
+
