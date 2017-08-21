94f17a6891603aba452a854c179d34a64d8d0f83
root <marcosptf@yahoo.com.br>
2017-08-21 07:13:54 -0300

    running examples flask-admin

--- python/flask-admin-labs/flask-admin-labs.py
+++ python/flask-admin-labs/flask-admin-labs.py
@@ -1,12 +1,20 @@
 """
-+Capitulo 9 - Flask Login + Flask Admin + Flask Redis + Memcached + PostgreSql
++Capitulo 9 - 
+Flask Login + Flask Admin + Flask Redis + Memcached + PostgreSql
 https://aws.amazon.com/pt/elasticache/what-is-redis/
 https://pypi.python.org/pypi/Flask-And-Redis/0.7
 https://pypi.python.org/pypi/Flask-Cache-Redis-Cluster/0.0.5
 https://pypi.python.org/pypi/Flask-PyMemcache/0.0.5
 https://pypi.python.org/pypi/Flask-Login/0.4.0
 https://pypi.python.org/pypi/Flask-Admin/1.5.0
+
++tentar ver quais os de exemplos podem nos ajudar para criar um 
+blog completo com camada de cache/login/admin:
+layout
+layout_bootstrap3 - testar
+auth-flask-login - testar
+
+
 """
 
-from Flask import flask
 
