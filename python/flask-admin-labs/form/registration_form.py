c16b60d9a83b9ec3d41f8aa2a6e0b04527d3f7e0
root <marcosptf@yahoo.com.br>
2017-08-28 23:24:50 -0300

    new arch flask-login-arch

---
+++ python/flask-admin-labs/form/registration_form.py
@@ -0,0 +1,12 @@
+
+from wtforms import form, fields, validators
+
+class RegistrationForm(form.Form):
+    login = fields.StringField(validators=[validators.required()])
+    email = fields.StringField()
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        if db.session.query(User).filter_by(login=self.login.data).count() > 0:
+            raise validators.ValidationError('Duplicate username')
+
