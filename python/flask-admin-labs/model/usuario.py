4895559a56704ea0ad59349b63cf9431e417916f
marcosptf <marcosptf@yahoo.com.br>
2017-09-13 13:01:15 -0300

    Update usuario.py

--- python/flask-admin-labs/model/usuario.py
+++ python/flask-admin-labs/model/usuario.py
@@ -6,18 +6,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Table, Column, Integer, String
+from model import postgres as pg
 
-user='postgres'
-password=''
-db='postgres'
-host='localhost'
-port=5432
-url = 'postgresql://{}:{}@{}:{}/{}'
-
-url = url.format(user, password, host, port, db)
-engine = sqlalchemy.create_engine(url, client_encoding='utf8')
-metadata = sqlalchemy.MetaData(bind=engine, reflect=True)
-#engine = create_engine("mysql://root:123456@localhost:3306/sistema_de_contatos", echo=True)
+metadata = pg.obtem_metadata_postgres()
+engine = pg.obtem_engine_postgres()
 Base = declarative_base()
 
 class Usuario(Base):
@@ -32,10 +24,23 @@ class Usuario(Base):
 
     def __repr__(self):
         return """
-        <Usuario(nome='%s', login='%s', email='%s', senha='%s')>
-        """ % (self.nome, self.login, self.email, self.senha)
-        return """
         <Usuario(id='%s', nome='%s', login='%s', email='%s', senha='%s')>
         """ % (self.id, self.nome, self.login, self.email, self.senha)
 
+    # Flask-Login integration
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    # Required for administrative interface
+    def __unicode__(self):
+        return self.nome
 
