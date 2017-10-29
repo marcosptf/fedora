24f3b8fcc44588ce3cc77ee1b93c86a8beef23c7
root <marcosptf@yahoo.com.br>
2017-10-29 06:17:59 -0200

    creating and coding flask babel labs

--- python/flask-babel-labs/flask-babel-labs.py
+++ python/flask-babel-labs/flask-babel-labs.py
@@ -1,1 +1,19 @@
+"""
+labs para 
+https://pythonhosted.org/Flask-Babel/
+"""
 
+
+from flask import Flask
+from flask.ext.babel import Babel
+from flask.ext.babel import gettext, ngettext
+
+app = Flask(__name__)
+app.config.from_pyfile('mysettings.cfg')
+#Babel(app=None, default_locale='en', default_timezone='UTC', date_formats=None, configure_jinja=True)
+babel = Babel(app, default_locale='pt_BR', default_timezone='BRST')
+
+
+gettext(u'A simple string')
+gettext(u'Value: %(value)s', value=42)
+ngettext(u'%(num)s Apple', u'%(num)s Apples', number_of_apples)
