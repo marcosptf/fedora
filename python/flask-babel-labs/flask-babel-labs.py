ad583ff8cced0b6363e5559ec22d282f9a2d6e00
root <marcosptf@yahoo.com.br>
2017-10-29 19:37:36 -0200

    trying working with flask babel :-(

--- python/flask-babel-labs/flask-babel-labs.py
+++ python/flask-babel-labs/flask-babel-labs.py
@@ -3,7 +3,6 @@ labs para
 https://pythonhosted.org/Flask-Babel/
 """
 
-
 from flask import Flask
 from flask.ext.babel import Babel
 from flask.ext.babel import gettext, ngettext
@@ -13,7 +12,9 @@ app.config.from_pyfile('mysettings.cfg')
 #Babel(app=None, default_locale='en', default_timezone='UTC', date_formats=None, configure_jinja=True)
 babel = Babel(app, default_locale='pt_BR', default_timezone='BRST')
 
-
 gettext(u'A simple string')
 gettext(u'Value: %(value)s', value=42)
 ngettext(u'%(num)s Apple', u'%(num)s Apples', number_of_apples)
+#$ pybabel extract -F babel.cfg -o messages.pot
+#$ pybabel init -i messages.pot -d translations -l de
+#$ pybabel compile -d translations
