84a1d1e21bcb79c7eb6adda8ff4f875c6289ef2e
root <marcosptf@yahoo.com.br>
2017-10-31 03:51:33 -0200

    code refactory flask-admin-blog

--- python/flask-admin-blog/config/config.py
+++ python/flask-admin-blog/config/config.py
@@ -1,9 +1,5 @@
-
-#postgresql+psycopg2://user:password@host:port/dbname[?key=value&key=value...]
-
 flask_app_config = {
   'SECRET_KEY' : '123456790',
   'SQLALCHEMY_DATABASE_URI' : 'postgresql://postgres:@localhost:5432/postgres',
   'SQLALCHEMY_ECHO' : True
 }
-
