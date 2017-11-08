da9164869b7ed17403a9bbd94ee879dbf39a4c71
marcosptf <marcosptf@yahoo.com.br>
2017-11-08 05:27:50 -0200

    Create yql-example.py

---
+++ python/yql-example.py
@@ -0,0 +1,10 @@
+import urllib2, urllib, json
+
+baseurl = "https://query.yahooapis.com/v1/public/yql?"
+yql_query = "select wind from weather.forecast where woeid=2460286"
+yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
+result = urllib2.urlopen(yql_url).read()
+data = json.loads(result)
+
+print(data['query']['results'])
+
