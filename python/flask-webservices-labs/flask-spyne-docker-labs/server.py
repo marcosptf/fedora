b4ee3989a2bd846742c8e86f425d13de440d90b0
marcosptf <marcosptf@yahoo.com.br>
2017-11-29 20:30:55 -0200

    Create server.py

---
+++ python/flask-webservices-labs/flask-spyne-docker-labs/server.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_spyne import Spyne
+from spyne.protocol.http import HttpRpc
+from spyne.protocol.soap import Soap11
+from spyne.model.primitive import Unicode, Integer
+from spyne.model.complex import Iterable, ComplexModel
+import logging
+
+h = logging.StreamHandler()
+rl = logging.getLogger()
+rl.setLevel(logging.DEBUG)
+rl.addHandler(h)
+
+app = Flask(__name__)
+spyne = Spyne(app)
+
+class AnswerServiceResponse(ComplexModel):
+    __namespace__ = 'tns'
+    dummy_str = Unicode(min_occurs=0, max_occurs=1, nillable=False)
+    dummy_num = Integer(min_occurs=1, max_occurs=1, nillable=True)
+
+class SomeSoapService(spyne.Service):
+    __service_url_path__ = '/soap/someservice'
+    __in_protocol__ = Soap11(validator='lxml')
+    __out_protocol__ = Soap11()
+
+    @spyne.srpc(Unicode, Integer, _returns=Iterable(Unicode))
+#    def echo(str, cnt):
+    def echo(str='hello world', cnt=0):
+        for i in range(cnt):
+            yield str
+
+    @spyne.srpc(Unicode, _returns=AnswerServiceResponse)
+    def answer(str):
+        return AnswerServiceResponse(dummy_str='answer is', dummy_num=42)
+
+class SomeSoapServiceTwo(spyne.Service):
+    __service_url_path__ = '/soap/someservicetwo'
+    __target_namespace__ = 'custom_namespacetwo'
+    __in_protocol__ = Soap11(validator='lxml')
+    __out_protocol__ = Soap11()
+
+    @spyne.srpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def echo(str, cnt):
+        for i in range(cnt):
+            yield str
+
+    @spyne.srpc(Unicode, _returns=AnswerServiceResponse)
+    def answer(str):
+        return AnswerServiceResponse(dummy_str='answer is', dummy_num=42)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1')
