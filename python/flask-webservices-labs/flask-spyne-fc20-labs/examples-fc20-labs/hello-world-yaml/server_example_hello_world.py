782ba6e108ad9d7ee2cd030341d43c4cb501061c
root <marcosptf@yahoo.com.br>
2018-01-21 19:02:37 -0200

    add hello world yaml flask-spyne

---
+++ python/flask-webservices-labs/flask-spyne-fc20-labs/examples-fc20-labs/hello-world-yaml/server_example_hello_world.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_spyne import Spyne
+from spyne.protocol.http import HttpRpc
+from spyne.protocol.yaml import YamlDocument
+from spyne.model.primitive import Unicode, Integer
+from spyne.model.complex import Iterable, ComplexModel
+import logging
+
+h = logging.StreamHandler()
+rl = logging.getLogger()
+rl.setLevel(logging.DEBUG)
+rl.addHandler(h)
+
+app = Flask(__name__)
+spyne = Spyne(app)
+
+class SomeJsonService(spyne.Service):
+    __service_url_path__ = '/yaml/anotherservice'
+    __in_protocol__ = HttpRpc(validator='soft')
+    __out_protocol__ = YamlDocument(ignore_wrappers=True)
+
+    @spyne.srpc(Unicode, Integer, _returns=Iterable(Unicode))
+    def echo(str, cnt):
+        for i in range(cnt):
+            yield str
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
