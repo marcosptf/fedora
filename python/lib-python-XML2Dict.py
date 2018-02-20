6ce4d8203833bb7946ed7b823bce3b14d657520a
marcosptf <marcosptf@yahoo.com.br>
2018-02-20 10:45:57 -0300

    Update lib-python-XML2Dict.py

--- python/lib-python-XML2Dict.py
+++ python/lib-python-XML2Dict.py
@@ -3,4 +3,5 @@
 from encoder import XML2Dict
 obj = XML2Dict("utf-8")
 print(obj.parse("<person><name>spring</name><age></age><address /></person>"))
-{'person': {'address': '', 'age': '', 'name': b'spring'}}
+#{'person': {'address': '', 'age': '', 'name': b'spring'}}
+obj['person']['address']
