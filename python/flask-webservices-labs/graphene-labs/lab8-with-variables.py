12e8653b545f38ea21e704d0f6af3f22c7caceb3
marcosptf <marcosptf@yahoo.com.br>
2018-04-22 07:07:20 -0300

    add a lot of examples graphene querys QL works fine

--- python/flask-webservices-labs/graphene-labs/lab8-with-variables.py
+++ python/flask-webservices-labs/graphene-labs/lab8-with-variables.py
@@ -13,27 +13,16 @@ Variables
 You can pass variables to a query via variable_values.
 """
 
-
-#http://docs.graphene-python.org/en/latest/execution/execute/
-import graphene
-
-"""
-For executing a query a schema, you can directly call the execute method on it.
-result represents the result of execution. 
-result.data is the result of executing the query, 
-result.errors is None if no errors occurred, and is a non-empty list if an error occurred.
-schema = graphene.Schema(...)
-result = schema.execute('{ name }')
-
-Variables
-You can pass variables to a query via variable_values.
-"""
-
-#pegar exemplo do lab5 para completar este ===>>>
+#exemplo sem variaveis funcionando ===>>>
 class User(graphene.ObjectType):
     id = graphene.ID()
     firstName = graphene.String()
     lastName = graphene.String()
+
+    def __init__(self, id, firstName, lastName):
+        self.id = 0
+        self.firstName = "java"
+        self.lastName = "scripter"
 
 class Query_0(graphene.ObjectType):
     user = graphene.Field(User)
@@ -51,15 +40,378 @@ query_ql_0 = '''
                lastName
             }
         }'''
-result = schema.execute(query_ql_0)
 
+result = schema.execute(query_ql_0)
+print("example=>0")
 print("result-simple-query-without-variable====>>>")
 print(result)
 print("result-data====>>>")
 print(result.data)
 print("result-data-user====>>>>")
 print(result.data['user'])
-print("\n")
+print("\n\n")
+
+
+
+
+#exemplo1 com variaveis variaveis funcionando ===>>>
+class User1(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+
+    def __init__(self, id, firstName, lastName):
+        self.id = 0
+        self.firstName = "java"
+        self.lastName = "scripter"
+
+class Query_1(graphene.ObjectType):
+    user = graphene.Field(User1)
+    
+    def resolve_user(self, info):
+        return info.context['user']
+
+schema = graphene.Schema(Query_1)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                id
+                firstName
+                lastName
+            }
+        }'''
+
+result = schema.execute(query_ql_2, context_value={'user' : User1(id=1, firstName="java", lastName="scripter" )})
+print("example=>1")
+print("result-simple-query-with-variable====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+#exemplo2 com variaveis variaveis funcionando ===>>>
+class User2(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+
+    def __init__(self, id, firstName, lastName):
+        self.id = 0
+        self.firstName = "java"
+        self.lastName = "scripter"
+
+class Query_2(graphene.ObjectType):
+    user = graphene.Field(User2)
+    
+    def resolve_user(self, info):
+        return info.context.get('user')
+
+schema = graphene.Schema(Query_2)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                id
+                firstName
+                lastName
+            }
+        }'''
+
+result = schema.execute(query_ql_2, context_value={'user' : User2(id=1, firstName="java", lastName="scripter" )})
+print("example=>2")
+print("result-simple-query-with-variable-using return info.context.get('user') ====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+#exemplo3 com variaveis variaveis funcionando ===>>>
+class User3(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+
+    def __init__(self, id, firstName, lastName):
+        self.id = 0
+        self.firstName = "java"
+        self.lastName = "scripter"
+
+class Query_3(graphene.ObjectType):
+    user = graphene.Field(User3)
+    
+    def resolve_user(self, info):
+        return info.context.get('user')
+
+schema = graphene.Schema(Query_3)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                id
+                firstName
+                lastName
+            }
+        }'''
+
+result = schema.execute(query_ql_2, context_value={'user' : User3(id=1, firstName="java", lastName="scripter" )})
+print("example=>3")
+print("result-simple-query-with-variable-using return info.context.get('user') ====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+#exemplo4 com variaveis variaveis funcionando ===>>>
+class User4(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+
+    def __init__(self, id, firstName, lastName):
+        #variables
+        #self.id = id
+        #self.firstName = firstName
+        #self.lastName = lastName
+        
+        #hard-coding
+        self.id = 0
+        self.firstName = "python"
+        self.lastName = "python-3.6"
+
+class Query_4(graphene.ObjectType):
+    user = graphene.Field(User4)
+    
+    def resolve_user(self, info):
+        return info.context.get('user')
+        #return User4(id=0, firstName="fname", lastName="lname")
+
+schema = graphene.Schema(Query_4)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                id
+                firstName
+                lastName
+            }
+        }'''
+
+result = schema.execute(query_ql_2, context_value={'user' : User4(id=None, firstName=None, lastName=None )})
+print("example=>4")
+print("result-simple-query-with-variable-using using variables in queryQL ====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+#exemplo5 com variaveis variaveis funcionando ===>>>
+class User5(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+
+    def __init__(self, id, firstName, lastName):
+        #variables
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        
+        #hard-coding
+        #self.id = 0
+        #self.firstName = "python"
+        #self.lastName = "python-3.6"
+
+class Query_5(graphene.ObjectType):
+    user = graphene.Field(User5)
+    
+    def resolve_user(self, info):
+        return info.context.get('user')
+        #return User4(id=0, firstName="fname", lastName="lname")
+
+schema = graphene.Schema(Query_5)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                id
+                firstName
+                lastName
+            }
+        }'''
+
+result = schema.execute(query_ql_2, context_value={'user' : User5(id=0, firstName="pylabs", lastName="py-labs3" )})
+print("example=>5")
+print("result-simple-query-with-variable-using using variables in queryQL ====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+#exemplo6 com variaveis variaveis funcionando ===>>>
+class User6(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+    street = graphene.String()
+    city = graphene.String()
+    state = graphene.String()
+    country = graphene.String()
+
+    def __init__(self, id, firstName, lastName, street, city, state, country):
+        #variables
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.street = street
+        self.city = city
+        self.state = state
+        self.country = country
+        
+        #hard-coding
+        #self.id = 0
+        #self.firstName = "python"
+        #self.lastName = "python-3.6"
+
+class Query_6(graphene.ObjectType):
+    user = graphene.Field(User6)
+    
+    def resolve_user(self, info):
+        return info.context.get('user')
+        #return User4(id=0, firstName="fname", lastName="lname")
+
+schema = graphene.Schema(Query_6)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                street
+                firstName
+                lastName
+                city
+            }
+        }'''
+
+user_instance = User6(id=0, firstName="pylabs", lastName="py-labs3", street="st", city="ct", state="stt", country="ctry" )
+result = schema.execute(query_ql_2, context_value={'user' : user_instance })
+print("example=>6")
+print("result-simple-query-with-variable-using using variables in queryQL ====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+#exemplo7 com variaveis variaveis funcionando ===>>>
+class User7(graphene.ObjectType):
+    id = graphene.ID()
+    firstName = graphene.String()
+    lastName = graphene.String()
+    street = graphene.String()
+    city = graphene.String()
+    state = graphene.String()
+    country = graphene.String()
+
+    def __init__(self, id, firstName, lastName, street, city, state, country):
+        #variables
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.street = street
+        self.city = city
+        self.state = state
+        self.country = country
+        
+        #hard-coding
+        #self.id = 0
+        #self.firstName = "python"
+        #self.lastName = "python-3.6"
+
+class Query_7(graphene.ObjectType):
+    user = graphene.Field(User6)
+    
+    def resolve_user(self, info):
+        return info.context.get('user')
+        #return User4(id=0, firstName="fname", lastName="lname")
+
+schema = graphene.Schema(Query_7)
+
+query_ql_2 = '''
+        query getUser {
+            user {
+                street
+                firstName
+                lastName
+                city
+            }
+        }'''
+
+
+user_instance = User7(id=0, firstName="pylabs", lastName="py-labs3", street="st", city="ct", state="stt", country="ctry" )
+result = schema.execute(query_ql_2, context_value={'user' : user_instance })
+print("example=>7")
+print("result-simple-query-with-variable-using using variables in queryQL ====>>>")
+print(result)
+print("result-data====>>>")
+print(result.data)
+print("result-data-user====>>>>")
+print(result.data['user'])
+print("\n\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
