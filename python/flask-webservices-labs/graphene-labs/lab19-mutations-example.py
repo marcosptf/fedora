2806b708bb3f59dba8d31816cb1916ca5e10559f
marcosptf <marcosptf@yahoo.com.br>
2018-04-23 05:51:37 -0300

    trying undertand mutations to praphene

--- python/flask-webservices-labs/graphene-labs/lab19-mutations-example.py
+++ python/flask-webservices-labs/graphene-labs/lab19-mutations-example.py
@@ -1,28 +1,58 @@
 import graphene
 
+result = None
+
 #class GeoInput with graphene InputObjectType, this class is to set or get latlng
 class GeoInput(graphene.InputObjectType):
+    
     #two properties required
-    lat = graphene.Float(required=True)
-    lng = graphene.Float(required=True)
+    lat = graphene.Float(required=False)
+    lng = graphene.Float(required=False)
 
     #another properties that return lat+lng
     @property
     def latlng(self):
         return "({},{})".format(self.lat, self.lng)
 
+
+
 #Address ObjectType graphene with properties latlng string
 class Address(graphene.ObjectType):
+    
+    #properties used on queryQL to answer
     latlng = graphene.String()
+    
+    def __init__(latlng):
+        result = latlng
+        
+    def resp():
+        return result
+
+
+
+
 
 #query ObjectType graphene that has a properties address that use address class and GeoInput class required
 class Query(graphene.ObjectType):
-    address = graphene.Field(Address, geo=GeoInput(required=True))
+    
+    #this properties is used to perform queryQL, is required lat, lng parameters;
+    address = graphene.Field(Address, geo=GeoInput(required=False))
 
     #execute address field given info and geo variables
     def resolve_address(self, info, geo):
+        
         #this return set latlng  string to Address class
-        return Address(latlng=geo.latlng)
+        #return Address(latlng=geo.latlng)
+        print("debugger-geo.latlng====>>>")
+        print(geo.latlng)
+        if geo.latlng!="(None,None)":
+            print("debugger->if")
+            return Address(latlng=geo.latlng)
+        print("debugger->None")
+        add = Address()
+        return add.resp()    
+
+
 
 #mutation graphene class 
 class CreateAddress(graphene.Mutation):
@@ -30,8 +60,9 @@ class CreateAddress(graphene.Mutation):
     #another class inside CreateAddress class, maybe it is a property?
     #this name Arguments is not call any place!
     class Arguments:
+
         #properties geo type GeoInput required
-        geo = GeoInput(required=True)
+        geo = GeoInput(required=False)
 
     #this is a instance from Address to Output?
     #this name Output is not call any place!
@@ -42,11 +73,15 @@ class CreateAddress(graphene.Mutation):
         #return class Address setting latlng to new value geo type GeoInput
         return Address(latlng=geo.latlng)
 
+
+
 #class mutation type ObjectType
 class Mutation(graphene.ObjectType):
     #use createAddress class to create new Address
     #maybe CreateAddress.Field() is call class Arguments from CreateAddress class?
     create_address = CreateAddress.Field()
+
+
 
 #mutations querys
 mutation = '''
@@ -57,6 +92,8 @@ mutation = '''
     }
 '''
 
+
+
 #query getting addres from some lat and lng
 query = '''
     query something{
@@ -66,9 +103,24 @@ query = '''
     }
 '''
 
+query1 = '''
+    query something{
+      address(geo: {lat:44.7, lng:77}) {
+        latlng
+      }
+    }
+'''
+
+query2 = '''
+    query something{
+      address(geo: {}) {
+        latlng
+      }
+    }
+'''
+
 #instance from new schema to query and mutation
 schema = graphene.Schema(query=Query, mutation=Mutation)
-
 
 #def test_query():
     #result = schema.execute(query)
@@ -90,17 +142,17 @@ schema = graphene.Schema(query=Query, mutation=Mutation)
     #}
 
 
-if __name__ == '__main__':
-    print("starting app mutations example lab19==>>>")
-    print("running query=>>")
-    result = schema.execute(query)
-    print("resp query====>>>")
-    print(result.data['address']['latlng'])
-    print("running mutation ===>>>")
-    schema.execute(schema.execute(mutation))
-    print("running query again===>>>")
-    result = schema.execute(query)
-    print("resp query 2 ===>>>>")
-    print(result.data['address']['latlng'])
+#if __name__ == '__main__':
+print("starting app mutations example lab19==>>>")
+print("running query=>>")
+result = schema.execute(query1)
+print("resp query====>>>")
+print(result.data['address']['latlng'])
+print("running mutation ===>>>")
+schema.execute(schema.execute(mutation))
+print("running query again===>>>")
+result = schema.execute(query2)
+print("resp query 2 ===>>>>")
+print(result.data['address']['latlng'])
 
 
