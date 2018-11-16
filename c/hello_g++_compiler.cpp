02d9bfd25449870a8fa4c289d91c1503166bee44
marcosptf <marcosptf@yahoo.com.br>
2018-11-15 22:03:42 -0200

    commit g++ files debuggers

---
+++ c/hello_g++_compiler.cpp
@@ -0,0 +1,22 @@
+#primeiro programa em c++ complilado com g++
+#
+#g++ -v -Wall -ohello_g++ hello_g++_compiler.cpp
+#
+#g++ => compilador gcc para c++
+#-v => verbose mode
+#-Wall => habilita mode de show all warnings
+#-ohello_g++ => nome do executavel compilado
+#
+#
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello g++";
+    return 0;
+}
+
+
+
